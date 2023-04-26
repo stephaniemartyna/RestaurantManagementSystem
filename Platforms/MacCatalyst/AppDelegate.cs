04ed2bd8ff7bd1556bd89acb68770fdfb15b749e
stephaniemartyna <119081894+stephaniemartyna@users.noreply.github.com>
2023-04-26 12:40:43 -0600

    Add files via upload

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RestaurantManagementSystem;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
